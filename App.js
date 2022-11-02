5ce57872a2c00809f168c8e19e7cdddc8bbc97b7
CHATHUMI <it20385064@my.sliit.lk>
2022-11-02 16:10:19 +0530

    driverscanner added

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import 'react-native-gesture-handler';
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
